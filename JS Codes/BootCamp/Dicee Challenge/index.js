bea202a8cdc4fa11f4da05812e9dbd2dc2989bb2
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-07-27 22:51:48 +0530

    Dice Challenge

---
+++ JS Codes/BootCamp/Dicee Challenge/index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document
+  .querySelector('img.img1')
+  .setAttribute('src', './images/dice' + randomNumber1 + '.png');
+document
+  .querySelector('img.img2')
+  .setAttribute('src', './images/dice' + randomNumber2 + '.png');
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = '🚩 Player 1 Wins!';
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector('h1').innerHTML = '🚩 Player 2 Wins!';
+} else {
+  document.querySelector('h1').innerHTML = 'Draw!';
+}
